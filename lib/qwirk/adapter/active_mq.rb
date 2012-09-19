c1950d3ba2e8bcffadf36db315db5b074bba8e6e
Brad Pardee <bradpardee@gmail.com>
2012-09-19 13:38:39 -0400

    Various fixes and better shutdown logic

--- lib/qwirk/adapter/active_mq.rb
+++ lib/qwirk/adapter/active_mq.rb
@@ -2,8 +2,6 @@ require 'activemq'
 require 'qwirk/adapter/active_mq/publisher'
 require 'qwirk/adapter/active_mq/worker_config'
 
-puts 'loading active_mq'
-
 ::Qwirk.register_adapter(:active_mq, ::Qwirk::Adapter::ActiveMQ::Publisher, ::Qwirk::Adapter::ActiveMQ::WorkerConfig) do |config|
   ::Qwirk::Adapter::JMS::Connection.new(config)
 end
--- lib/qwirk/adapter/active_mq/worker_config.rb
+++ lib/qwirk/adapter/active_mq/worker_config.rb
@@ -3,6 +3,11 @@ module Qwirk
   module Adapter
     module ActiveMQ
       class WorkerConfig < JMS::WorkerConfig
+        def self.in_process?(config)
+          broker_url = config[:broker_url]
+          return !!(broker_url && broker_url.start_with?('vm://'))
+        end
+
         def init
           if self.topic_name
             self.queue_name = "Consumer.#{self.name}.VirtualTopic.#{topic_name}"
